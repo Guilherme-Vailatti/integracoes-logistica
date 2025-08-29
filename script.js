47dfa316f4d86c76e0460789298c72f23da4d970
Guilherme Vailatti <60483961+Guilherme-Vailatti@users.noreply.github.com>
2025-08-29 11:49:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -32,9 +32,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <br><br><b>Integrações:</b>
       <ul>
         <li><b>ERP Senior:</b> permite a troca de informações essenciais entre os sistemas de gestão de armazém e de processos administrativos. Essa integração ocorre via Senior X, utilizando a tecnologia API REST, e requer o uso de componentes ETL, além de ERP Service. <br><a href="https://documentacao.senior.com.br/gestao-armazenagem-wms-wis/6.5/#manual-processos-wis/integracao-entre-sistemas/wis-erp.htm%3FTocPath%3DManual%2520Integra%25C3%25A7%25C3%25A3o%7CIntegra%25C3%25A7%25C3%25B5es%2520com%2520outros%2520sistemas%7C_____2">Saiba mais</a>.</li>
-        <li><b>Gestão de Pátio | YMS:</b> sincroniza as operações do armazém com os processos de controle de pátio. Essa integração é feita via Senior X, utilizando a tecnologia API REST e exige a presença de componentes ETL.</li>
-        <li><b>Gestão de Mão de Obra | LMS:</b> Integração em construção.</li>
-        <li><b>Roteirizador:</b> Integração em construção.</li>
+        <li><b>Roteirizador:</b> tem como objetivo automatizar o processo de roteirização de pedidos, otimizando a formação de cargas no sistema Roteirização. <br><a href="https://documentacao.senior.com.br/gestao-armazenagem-wms-wis/6.5/#manual-processos-wis/integracao-entre-sistemas/wis-tms.htm%3FTocPath%3DManual%2520Integra%25C3%25A7%25C3%25A3o%7CIntegra%25C3%25A7%25C3%25B5es%2520com%2520outros%2520sistemas%7C_____3">Saiba mais</a>.</li>
       </ul>`
     },
 
@@ -53,8 +51,7 @@ document.addEventListener("DOMContentLoaded", function () {
       <ul>
         <li><b>ERP Senior:</b> garante a troca de informações entre as operações logísticas do armazém e os processos de gestão empresarial. A integração é feita via Senior X, utilizando a tecnologia API REST, e exige a presença dos componentes ETL ERP e ERP Service para seu pleno funcionamento. <br><a href="https://documentacao.senior.com.br/wms-alcis/13.0/#wms-13/manuais-integracao/integracao-erp.htm%3FTocPath%3DSenior%2520WMS%2520Alcis%2520Web%2520-%2520Manual%2520do%2520Usu%25C3%25A1rio%7CManuais%2520t%25C3%25A9cnicos%2520e%2520integra%25C3%25A7%25C3%25B5es%7CManuais%2520de%2520integra%25C3%25A7%25C3%25A3o%2520%25E2%2580%2593%2520Senior%2520WMS%2520Alcis%7C_____3">Saiba mais</a>.</li>
         <li><b>Acesso e Segurança:</b> envia dados de motoristas, veículos, pessoas e agendamentos por meio de eventos da SAM API, como criação e atualização de cadastros e agendamentos de acesso. <br><a href="https://documentacao.senior.com.br/seniorxplatform/manual-do-usuario/yms/#manual-processos/gestao-patio-agendamento/gestao-de-agendamento/integracao/integracao-ronda.htm%3FTocPath%3DGest%25C3%25A3o%2520de%2520P%25C3%25A1tio%2520-%2520Manual%2520do%2520Usu%25C3%25A1rio%7CIntegra%25C3%25A7%25C3%25B5es%7C_____2">Saiba mais</a>.</li>
-        <li><b>WMS WIS:</b> Integração em construção.</li>
-        <li><b>WMS Senior:</b> Integração em construção.</li>
+        <li><b>WMS Senior:</b> oferece uma nova abordagem para operações logísticas, especialmente para processos de descarga. Essa integração utiliza tecnologia API REST. <br><a href="https://documentacao.senior.com.br/seniorxplatform/manual-do-usuario/yms/#manual-processos/gestao-patio-agendamento/gestao-de-agendamento/integracao/integracao-wms.htm%3FTocPath%3DGest%25C3%25A3o%2520de%2520P%25C3%25A1tio%2520-%2520Manual%2520do%2520Usu%25C3%25A1rio%7CIntegra%25C3%25A7%25C3%25B5es%7C_____3">Saiba mais</a>.</li>
 
 
         </ul>`
@@ -70,12 +67,11 @@ document.addEventListener("DOMContentLoaded", function () {
         </ul>`
       },
       "gestao-mao-de-obra": { titulo: "Gestão de Mão de Obra | LMS",
-      descricao: `Texto Texto Texto Texto Texto.
+      descricao: `É uma solução de gerenciamento de trabalho, focada na gestão do capital humano em relação a tarefas, recursos e equipamentos, com base nas movimentações de centros de distribuição..
       <br><br><b>Integrações:</b>
       <ul>
-        <li><b>WMS Senior</b> texto texto texto texto texto texto.</li>
-        <li><b>WMS Alcis</b> texto texto texto texto texto texto.</li>
-        <li><b>WMS WIS</b> texto texto texto texto texto texto.</li> 
+        <li><b>WMS Senior</b>: integração via Senior X, utiliza tecnologia API REST exigindo componentes ETL.</li>
+        <li><b>WMS Alcis</b>: utiliza tecnologia API REST.</li>
 
         </ul>`
       },
@@ -86,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
         <li><b>Torre de Controle:</b> integra com o Torre de Controle para receber atividades, pedidos e notas, permitindo a roteirização eficiente das entregas. <br><a href="https://documentacao.senior.com.br/roteirizacaoemonitoramento/7.0.0/">Saiba mais</a>.</li> 
         <li><b>ERP Senior X:</b> integra com o ERP Senior por meio do Senior X para receber atividades, pedidos e notas, viabilizando a roteirização eficiente.</li> 
         <li><b>TMS:</b> integra com a TMS Transportador para receber atividades, pedidos e notas, permitindo a roteirização eficiente das entregas.</li> 
-        <li><b>WMS Senior:</b> Integração em construção.</li>
+        <li><b>WMS Senior:</b> oferece integração de atividades, pedidos e notas para roteirizar e planejar as rotas de entrega. <br><a href="https://documentacao.senior.com.br/gestao-armazenagem-wms-wis/6.5/#manual-processos-wis/integracao-entre-sistemas/wis-tms.htm%3FTocPath%3DManual%2520Integra%25C3%25A7%25C3%25A3o%7CIntegra%25C3%25A7%25C3%25B5es%2520com%2520outros%2520sistemas%7C_____3">Saiba mais</a>.</li>
         </ul>`
       },
       "torre-controle": { titulo: "Torre de Controle - RMS",
@@ -107,8 +103,8 @@ document.addEventListener("DOMContentLoaded", function () {
       <ul>
         <li><b>Torre de Controle - RMS:</b> troca de atividades, pedidos e notas, acompanhando a execução e controle da entrega via Torre de Controle.</li> 
         <li><b>ERP Senior X:</b> integra com a WMS Senior para troca de atividades, pedidos e notas, acompanhando a execução via Torre de Controle e a execução de entrega. <br><a href="https://documentacao.senior.com.br/roteirizacaoemonitoramento/7.0.0/#integracoes/intregracao-rms-wms.htm%3FTocPath%3DIntegra%25C3%25A7%25C3%25B5es%2520RMS%7C_____2">Saiba mais</a>.</li> 
-        <li><b>TMS:</b> Integração em construção.</li>
-        <li><b>WMS Senior:</b> Integração em construção.</li>
+        <li><b>TMS:</b> oferece integração de atividades, pedidos e notas para acompanhamento da execução das entregas por meio da Torre de Controle.</li>
+        <li><b>WMS Senior:</b> oferece Integração de atividades, pedidos e notas com a Torre de Controle para acompanhamento da execução das entregas, tendo como premissa a utilização do roteirizador. A integração envia o status do processo e recebe informações como atividades, pedidos, notas fiscais, alterações, cancelamentos, confirmações de execução e dados dos veículos.</li>
         </ul>`
       },
        "gko-frete": { titulo: "GKO Frete",
@@ -164,6 +160,18 @@ document.addEventListener("DOMContentLoaded", function () {
       <ul>
         <li><b>Gestão de Pátio | YMS:</b> texto texto.</li> 
         </ul>`
+      },
+      "plataforma-frete": { titulo: "Plataforma de Frete",
+      descricao: `</b>
+      <ul>
+        <li><b>GKO Frete:</b> integração via WebHook para recebimento de Pedidos, Notas Fiscais, Faturas, e integração via API para envio das ofertas como Frete Combinado e envio de CTE. <br><a href="https://documentacao.senior.com.br/plataforma-fretes/1.0/#manual-processos/integracao-gko-frete.htm%3FTocPath%3DPlataforma%2520de%2520Fretes%2520-%2520Manual%2520de%2520Usu%25C3%25A1rio%7CIntegra%25C3%25A7%25C3%25B5es%7C_____1">Saiba mais</a></li> 
+        </ul>`
+      },
+      "confirma-facil": { titulo: "Confirma Fácil",
+      descricao: `</b>
+      <ul>
+        <li><b>GKO Frete:</b> integração de Notas Fiscais via API e CSV. Integração de Ocorrência via CSV.</li> 
+        </ul>`
       }
   };
 
@@ -177,7 +185,7 @@ function resetVisualizacao() {
 }
 
 // Clique em ícones com a classe .item
-document.querySelectorAll(".item").forEach(item => {
+document.querySelectorAll(".item, .new-items").forEach(item => {
   item.addEventListener("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
